91f581e499b55c31fef644cc2795d8db67ffdcf8
kimjongbing <126769537+kimjongbing@users.noreply.github.com>
2023-07-30 23:21:29 +1000

    massive changes, semi functional site: - seperated frontend and backend folders - made rust backend convert md files to display on the site - laid the groundwork for now

---
+++ backend/src/main.rs
@@ -0,0 +1,63 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+use rocket::response::content;
+use rocket::response::NamedFile;
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::env;
+
+use comrak::{markdown_to_html, ComrakOptions};
+
+fn get_frontend_directory() -> PathBuf {
+    let current_dir = env::current_dir().expect("Failed to get the current working directory.");
+    let mut frontend_dir = current_dir;
+    frontend_dir.pop();
+    frontend_dir.join("frontend")
+}
+
+#[get("/content.md")]
+fn content_md() -> content::Html<String> {
+    let frontend_dir = get_frontend_directory();
+    let relative_path = Path::new("docs/index.md");
+    let path = frontend_dir.join(relative_path);
+    println!("Trying to read markdown from: {:?}", path);
+
+    match fs::read_to_string(&path) {
+        Ok(markdown) => {
+            let html_content = markdown_to_html(&markdown, &ComrakOptions::default());
+            content::Html(html_content)
+        },
+        Err(err) => {
+            println!("Error reading markdown file: {}", err);
+            content::Html("Error reading markdown file".to_string())
+        },
+    }
+}
+
+#[get("/<file..>", rank = 2)]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    let frontend_dir = get_frontend_directory();
+    let path = frontend_dir.join(file);
+    println!("Trying to open file at: {:?}", path);
+    NamedFile::open(path).ok()
+}
+
+#[get("/", rank = 1)]
+fn index() -> Option<NamedFile> {
+    let frontend_dir = get_frontend_directory();
+    let path = frontend_dir.join("index.html");
+    println!("Trying to open index at: {:?}", path);
+
+    match NamedFile::open(path) {
+        Ok(file) => Some(file),
+        Err(err) => {
+            println!("Error opening file: {}", err);
+            None
+        },
+    }
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index, files, content_md]).launch();
+}
---
+++ frontend/docs/index.md
@@ -0,0 +1,13 @@
+# Welcome
+This is my website where I hope to talk about my projects and other things I find interesting. I would also like to use this as a place to document my learning process and share my knowledge with others. Furthermore, I would like to improve my UI/UX skills and this website is a great place to do that as so far I have been only working on backend projects.
+
+## Socials
+- [**GitHub**](https://github.com/kimjongbing)
+
+## Projects
+- [**Proxy Checker**](https://github.com/kimjongbing/JAPCproxychecker) - A simple proxy checker written in Python that is also capable of grabbing proxies from various sources that can be customized.
+- [**SHA1 Cracker**](https://github.com/kimjongbing/sha1_cracker) - A simple SHA1 cracker written in Rust. 
+- [**DLL Injector**](https://github.com/kimjongbing/dll_injector) - A relatively simple DLL injector I made in Rust.
+- [**UWP DLL Injector**](https://github.com/kimjongbing/dll_injector_uwp) - A more advanced DLL injector I created in both Rust and C++ which is capable of injecting into Universal Windows Platform (UWP) applications.
+
+## Blogs
